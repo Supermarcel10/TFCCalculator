836546ad2ecfb29303936db2ee3fb97507ad38d0
marcel <47798787+Supermarcel10@users.noreply.github.com>
2025-09-27 06:13:39 +0100

    - Added addVariants to AvailableMineralBuilder

--- test/helpers/availableMineralBuilder.ts
+++ test/helpers/availableMineralBuilder.ts
@@ -10,11 +10,28 @@ export class AvailableMineralBuilder {
 			name : "",
 			produces : mineralType,
 			yield : yieldUnits,
-			quantity,
+			quantity : quantity,
 			uses : [MineralUseCase.Vessel, MineralUseCase.Vessel]
 		};
 
 		return this.addType(mineralType, [quantifiedMineral]);
+	}
+
+	addVariants(mineralType : string, variants : number = 10, baseYield : number = 12, quantityEach : number = 60) : this {
+		const minerals : QuantifiedMineral[] = new Array(variants);
+		const incrementSequence = this.getFibonacciSequence(variants);
+
+		for (let i = 0; i < variants; ++i) {
+			minerals[i] = {
+				name : "",
+				produces : mineralType,
+				yield : baseYield + incrementSequence[i],
+				quantity : quantityEach,
+				uses : [MineralUseCase.Vessel, MineralUseCase.Vessel]
+			};
+		}
+
+		return this.addType(mineralType, minerals);
 	}
 
 	addNoise(amount : number = 50) : this {
@@ -27,7 +44,17 @@ export class AvailableMineralBuilder {
 		for (const [type, arr] of this.entries) {
 			m.set(type.toLowerCase(), arr);
 		}
+
 		return m;
+	}
+
+	private getFibonacciSequence(count : number) {
+		const fib = [0, 1];
+		for (let i = 2; i < count; ++i) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+
+		return fib.slice(0, count);
 	}
 
 	private addType(mineralType : string, minerals : QuantifiedMineral[]) : this {
