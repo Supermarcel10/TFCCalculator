e0f8f7d9f51d82823dd1379a4a9a8160c19738ef
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-05-17 23:17:33 +0100

    - Update shell.nix with rust setup

--- shell.nix
+++ shell.nix
@@ -2,10 +2,22 @@
 
 pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
+        # START General
         git
         gh
+        # END General
+
+        # START Node.js
         pnpm
         nodejs_22
+        # END Node.js
+
+        # START Rust
+        cargo
+        rustc
+        rustfmt
+        clippy
+        # END Rust
     ];
 
     shellHook = ''
