808e39f024f3e2878326f7c59ead886390961acd
G'lek Tarssza <glek@glektarssza.com>
2026-06-30 18:10:33 -0600

    More goof fixing.

--- src/services/calculation/OutputCalculator.ts
+++ src/services/calculation/OutputCalculator.ts
@@ -1,14 +1,13 @@
-import {IInputNormalizationService} from "./abstract/IInputNormalizationService";
-import {CalculationOutput, Flags, FlagValues, IOutputCalculator} from "@/services/calculation/abstract/IOutputCalculator";
-import {QuantifiedMineral, SmeltingComponent} from "@/types";
-import {InputNormalizationService} from "@/services/calculation/InputNormalizationService";
-import {IOutputResolutionStrategyExecutor} from "./abstract/IOutputResolutionStrategyExecutor";
-import OutputResolutionStrategyExecutor from "./OutputResolutionStrategyExecutor";
-
+import {IInputNormalizationService} from './abstract/IInputNormalizationService';
+import {CalculationOutput, Flags, FlagValues, IOutputCalculator} from '@/services/calculation/abstract/IOutputCalculator';
+import {QuantifiedMineral, SmeltingComponent} from '@/types';
+import {InputNormalizationService} from '@/services/calculation/InputNormalizationService';
+import {IOutputResolutionStrategyExecutor} from './abstract/IOutputResolutionStrategyExecutor';
+import OutputResolutionStrategyExecutor from './OutputResolutionStrategyExecutor';
 
 export class OutputCalculator implements IOutputCalculator {
-	private readonly inputNormalizationService : IInputNormalizationService;
-	private readonly outputResolutionStrategyExecutor : IOutputResolutionStrategyExecutor;
+	private readonly inputNormalizationService: IInputNormalizationService;
+	private readonly outputResolutionStrategyExecutor: IOutputResolutionStrategyExecutor;
 
 	constructor() {
 		this.inputNormalizationService = new InputNormalizationService();
@@ -16,12 +15,12 @@ export class OutputCalculator implements IOutputCalculator {
 	}
 
 	calculateSmeltingOutput(
-			targetMb : number,
-			components : SmeltingComponent[],
-			availableMinerals : Map<string, QuantifiedMineral[]>,
-			flags? : Flags,
-			flagValues? : FlagValues
-	) : CalculationOutput {
+		targetMb: number,
+		components: SmeltingComponent[],
+		availableMinerals: Map<string, QuantifiedMineral[]>,
+		flags?: Flags,
+		flagValues?: FlagValues
+	): CalculationOutput {
 		const normalizedComponents = this.inputNormalizationService.normalizeComponents(components);
 		const normalizedInv = this.inputNormalizationService.normalizeInventory(availableMinerals);
 
