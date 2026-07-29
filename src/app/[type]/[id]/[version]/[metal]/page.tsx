f9cf177e27f3557448b7270e3724003f462f66e8
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-07-29 23:53:13 +0100

    - Update back button

--- src/app/[type]/[id]/[version]/[metal]/page.tsx
+++ src/app/[type]/[id]/[version]/[metal]/page.tsx
@@ -52,7 +52,7 @@ export default function MetalPage() {
 							title={replaceUnderscoreWithSpace(metalString).toUpperCase()}
 							subheading={subheadingString}
 							ariaPreviousScreenName="metal selection"
-							handleBackURI={`/${type}/${id}/${version}/metals`}
+							handleBackURI="/"
 							dropdown={{
 								options: dropdownOptions,
 								selected: metalString,
