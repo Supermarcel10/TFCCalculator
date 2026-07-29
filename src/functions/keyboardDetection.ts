898deb9433e86656be680f7ed1428716206b53ed
marcel <47798787+Supermarcel10@users.noreply.github.com>
2026-07-29 23:58:33 +0100

    - Cleanup

--- src/functions/keyboardDetection.ts
+++
@@ -1,4 +0,0 @@
-export const hasPhysicalKeyboard = (): boolean => {
-  if (typeof window === 'undefined') return false;
-  return window.matchMedia('(pointer: fine) and (hover: hover)').matches;
-};
--- src/types/filters.ts
+++
@@ -1,5 +0,0 @@
-export enum CreationSelectionFilter {
-	All,
-	Metals,
-	Alloys
-}
